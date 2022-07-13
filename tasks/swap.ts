3144bd3c0615f95806ee1655b9b0139caea2666d
asimaranov <a.simaranov@gmail.com>
2022-07-13 18:33:30 +0300

    Fixed swap

--- tasks/swap.ts
+++ tasks/swap.ts
@@ -161,8 +161,8 @@ const main = async () => {
   console.log(`User account A: ${userAccountA.address.toBase58()}`)
   console.log(`User account B: ${userAccountB.address.toBase58()}`)
 
- await fetchedTokenSwap.swap(userAccountA.address, tokenAccountA.address, tokenAccountB.address, userAccountB.address, feeAccount.address, new Account(owner.secretKey), 5000, 5000)
- // Swap instruction exceeds desired slippage limit
+ await fetchedTokenSwap.swap(userAccountA.address, tokenAccountA.address, tokenAccountB.address, userAccountB.address, feeAccount.address, new Account(owner.secretKey), 5000, 1)
+
 }
 
 
